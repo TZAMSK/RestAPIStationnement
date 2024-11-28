9ae4cb703ac75bba6df4eec541d96139b785ecb9
Alikhan Khakimov <akhakimov@dept-info.crosemont.quebec>
2024-11-28 01:09:26 -0500

    Implémentation de get_stationnement (on stationnement spécifique avec le id)

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,35 @@ use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Charge les données dans .env
     dotenv().ok();
 
+    // La variable prend la valeur de DATABASE_URL qui est définit dans .env.
+    // DATABASE_URL est utilisé pendent le temps de l'execution et non temps de compilation
+    // Pour charger la configuration
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
+    // Instancier la connexion avec MySQL
     let pool = MySqlPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
+        // Connexion est asynchrone. Donc,
         .await
-        .expect("Error creating database connection pool");
+        .expect("Erreur de la création de la connexion ");
 
+    // Instancier la connexion avec le serveur Actix Web
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
+            // HTTP:GET TOUS LES STATIONNEMENTS
+            // http://localhost:8080/stationnements
             .service(service::get_stationnements)
+            // HTTP:GET UN STATIONNEMENT
+            // http://localhost:8080/stationnement
+            .service(service::get_stationnement)
     })
-    .bind("127.0.0.1:8080")?
+    // Adresse réseau avec le port 8080
+    .bind("localhost:8080")?
     .run()
     .await
 }
--- src/service.rs
+++ src/service.rs
@@ -1,11 +1,12 @@
 use crate::model::{Adresse, Coordonnee, Stationnement};
-use actix_web::{get, patch, web, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
 use serde_json::json;
 use sqlx::MySqlPool;
 
 #[get("/stationnements")]
 pub async fn get_stationnements(pool: web::Data<MySqlPool>) -> impl Responder {
-    // Mapping manually to nested struct using SQLx
+    // À la place de faire SELECT * FROM
+    // on spécifie les champs parce que la structure de mon modèle Stationnement est imbriqué
     let rows = sqlx::query!(
         r#"
         SELECT
@@ -21,13 +22,19 @@ pub async fn get_stationnements(pool: web::Data<MySqlPool>) -> impl Responder {
         FROM stationnements
         "#
     )
+    // On s'attend à recevoir toutes les lignes
+    // donc, all
     .fetch_all(pool.get_ref())
     .await;
 
     match rows {
+        // Si réussi à communiquer
         Ok(rows) => {
             let stationnements: Vec<Stationnement> = rows
+                // Avec la règle primère de Rust, into_iter prend possession des données et les
+                // met dans stationnements. donc row n'est plus valide
                 .into_iter()
+                // Transfomer ce qui est dans le vecteurs pour produire un stationnement
                 .map(|row| Stationnement {
                     id: row.id,
                     adresse: Adresse {
@@ -43,14 +50,74 @@ pub async fn get_stationnements(pool: web::Data<MySqlPool>) -> impl Responder {
                     heures_dispo: row.heures_dispo,
                     date_dispo: row.date_dispo,
                 })
+                // Collecter les données transformés vers le vecteurs stationnements
                 .collect();
 
+            // Si réussi, création de la requête
             HttpResponse::Ok().json(stationnements)
         }
+        // Sinon
         Err(e) => {
-            eprintln!("Error fetching stationnements: {}", e);
+            eprintln!("Erreur d'aller chercher les stationnements: {}", e);
             HttpResponse::InternalServerError().json(json!({
-                "error": "Failed to fetch stationnements"
+                "Erreur": "Erreur de recherche stationnements"
+            }))
+        }
+    }
+}
+
+#[get("/stationnements/{id}")]
+pub async fn get_stationnement(
+    path: web::Path<uuid::Uuid>,
+    pool: web::Data<MySqlPool>,
+) -> impl Responder {
+    let stationnement_id = path.into_inner().to_string();
+    let row = sqlx::query!(
+        r#"
+        SELECT
+            id,
+            numero_municipal,
+            rue,
+            code_postal,
+            longitude,
+            latitude,
+            panneau,
+            heures_dispo,
+            date_dispo
+        FROM stationnements
+        WHERE id = ?"#,
+        stationnement_id
+    )
+    // On s'attend à recevoir une seule ligne
+    .fetch_one(pool.get_ref())
+    .await;
+
+    match row {
+        // Si réussi à communiquer
+        Ok(row) => {
+            let stationnement = Stationnement {
+                id: row.id,
+                adresse: Adresse {
+                    numero_municipal: row.numero_municipal,
+                    rue: row.rue,
+                    code_postal: row.code_postal,
+                },
+                coordonnee: Coordonnee {
+                    longitude: row.longitude,
+                    latitude: row.latitude,
+                },
+                panneau: row.panneau,
+                heures_dispo: row.heures_dispo,
+                date_dispo: row.date_dispo,
+            };
+
+            HttpResponse::Ok().json(stationnement)
+        }
+        // Sinon
+        Err(e) => {
+            eprintln!("Erreur d'aller chercher les stationnements: {}", e);
+            HttpResponse::InternalServerError().json(json!({
+                "Erreur": "Erreur de recherche stationnements"
             }))
         }
     }
