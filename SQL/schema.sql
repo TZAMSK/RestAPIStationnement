497184033441859f17873508a89eb192766e95e4
Alikhan Khakimov <akhakimov@dept-info.crosemont.quebec>
2024-11-28 03:55:47 -0500

    Implémentation de get_stationnements_heure (cherche stationnement dans une plage d'heure)

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -4,13 +4,14 @@ CREATE DATABASE stationnementsdb;
 use stationnementsdb;
 
 CREATE TABLE stationnements (
-    id CHAR(36) PRIMARY KEY,                          -- Unique identifier for each stationnement
-    numero_municipal VARCHAR(50) NOT NULL,       -- Street number
-    rue VARCHAR(255) NOT NULL,                   -- Street name
-    code_postal VARCHAR(10) NOT NULL,            -- Postal code
-    longitude DOUBLE PRECISION NOT NULL,         -- Longitude coordinate
-    latitude DOUBLE PRECISION NOT NULL,          -- Latitude coordinate
-    panneau TEXT NOT NULL,                       -- Reference to the panneau (image or description)
-    heures_dispo VARCHAR(50) NOT NULL,           -- Hours available
-    date_dispo VARCHAR(50) NOT NULL              -- Date available
+    id CHAR(36) PRIMARY KEY,                          
+    numero_municipal VARCHAR(50) NOT NULL,       
+    rue VARCHAR(255) NOT NULL,                   
+    code_postal VARCHAR(10) NOT NULL,            
+    longitude DOUBLE PRECISION NOT NULL,         
+    latitude DOUBLE PRECISION NOT NULL,          
+    panneau TEXT NOT NULL,
+    heures_debut TIME NOT NULL,
+    heures_fin TIME NOT NULL,           
+    date_dispo VARCHAR(50) NOT NULL              
 );
